e0f9a967f78545fa7ac40d6e282d9e817158363f
Andrew Bush <apb2471@cs.rit.edu>
2023-02-26 01:08:05 -0500

    feat: Add UI features to chat

--- recieve_sms.py
+++ recieve_sms.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from textblob import TextBlob
+
+
+
+import csv
 
 app = Flask(__name__)
+
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -12,12 +18,29 @@ def incoming_sms():
     resp = MessagingResponse()
 
     # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
+    symptomsList = []
+    symptoms = []
+    choice = body.split(",", 1)[0].lower()
+    print(choice)
+    if choice == "about":
+        resp.message("You have reached MedText, a SMS application for receiving AI input on your symptoms")
+    elif choice == "symptoms":
+        reader = csv.reader(body.split('\n'), delimiter=',')
+        for row in reader:  # each row is a list
+            symptomsList.append(row)
+        for s in symptomsList[0][1:]:
+            j = s.lower().stripls
+            sentence = TextBlob(j)
+            result = sentence.correct()
+            symptoms.append(str(result))
+        resp.message("Symptoms received, we will get back to you in a moment...\n")
+    else:
+        resp.message("Invalid input for the MedTex service. To perform a query, enter 'symptoms' follow by a comma "
+                     "separated list of the symptoms you are experiencing. \n Ex: symptoms, headache,fever")
 
+    print(symptoms)
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
